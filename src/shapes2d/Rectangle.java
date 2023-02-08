3486736822e14d47aeebd992209cbba5e0f00117
Iván Piza <hpiza@iteso.mx>
2023-02-08 10:52:02 -0600

    Sesión 7

--- src/shapes2d/Rectangle.java
+++ src/shapes2d/Rectangle.java
@@ -2,8 +2,39 @@ package shapes2d;
 
 public class Rectangle {
 
-    public double base;
-    public double height;
+    private double base = 1;
+    private double height = 1;
+
+    public Rectangle() {
+        // this(3, 4);
+    }
+
+    public Rectangle(double side) {
+        this(side, side);
+    }
+
+    public Rectangle(double base, double height) {
+        setBase(base);
+        setHeight(height);
+    }
+
+    public double getBase() {
+        return this.base;
+    }
+
+    public void setBase(double base) {
+        if(base <= 0) return;
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        if(height <= 0) return;
+        this.height = height;
+    }
 
     public double area() {
         return base * height;
@@ -17,5 +48,13 @@ public class Rectangle {
         System.out.printf("(%.1f x %.1f)\n", base, height);
     }
 
+    @Override
+    public String toString() {
+        return String.format("[base=%.1f, height=%.1f, area=%.1f, perimeter=%.1f]", this.base, this.height, this.area(), this.perimeter());
+    }
+
+    public boolean equals1(Rectangle rect) {
+        return this.base == rect.getBase() && this.height == rect.getHeight();
+    }
 
 }
--- src/tests/TestRectangle.java
+++ src/tests/TestRectangle.java
@@ -2,16 +2,55 @@ package tests;
 
 import shapes2d.Rectangle;
 
+import javax.swing.*;
+
 public class TestRectangle {
 
     public static void main(String[] args) {
-        Rectangle r1 = new Rectangle();
-        Rectangle r2 = new Rectangle();
-        r1.base = 4;
-        r1.height = 3;
+        Rectangle r1 = new Rectangle(4, 3);     // base = 4, height = 3
+        //r1.setBase(4);
+        //r1.setHeight(3);
+        //r1.base = 4;
+        //r1.height = 3;
         r1.print();
+        System.out.println("Area de r1: " + r1.area());
+        System.out.println("Perímetro de r1: " + r1.perimeter());
 
+        Rectangle r2 = new Rectangle();     // base = 1, height = 1
+        r2.setBase(0);                      // base = 1, height = 1
+        r2.setHeight(-5);                   // base = 1, height = 1
+        r2.setBase(3);                      // base = 3, height = 1
+        r2.setBase(-3);                     // base = 3, height = 1
+        //r2.base = -2;
+        //r2.height = -5;
         r2.print();
+
+        Rectangle r3 = new Rectangle(0, -2);     // base = 1, height = 1
+        r3.print();
+        r3.setBase(2 * r1.getBase());                       // base = 8, height = 1
+        r3.print();
+        r3.setHeight(2 * r1.getHeight());                   // base = 8, height = 6
+        r3.print();
+
+        Rectangle r4 = new Rectangle(5);                // base = 5, height = 5
+        r4.print();
+
+        System.out.println("r1: " + r1);
+        System.out.println("r2: " + r2);
+        System.out.println("r3: " + r3);
+        System.out.println("r4: " + r4);
+
+        double a = r1.area();
+        String str1 = "El área de " + r1 + " es " + a;
+        System.out.println(str1);
+        //JOptionPane.showMessageDialog(null, r1);
+
+        Rectangle r5 = new Rectangle(5);
+        System.out.println("r5: " + r5);
+
+        System.out.println("¿r4 == r5? " + (r4 == r5));
+        System.out.println("¿r4.equals(r5)? " + (r4.equals1(r5)));
+
     }
 
 }

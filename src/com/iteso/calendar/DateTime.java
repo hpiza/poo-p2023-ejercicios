c3ae26317bc2e9d3763bccccecc52d085156aef7
Iván Piza <hpiza@iteso.mx>
2023-03-22 14:19:20 -0600

    DateTime

--- src/com/iteso/calendar/DateTime.java
+++ src/com/iteso/calendar/DateTime.java
@@ -1,2 +1,119 @@
-package com.iteso.calendar;public class DateTime {
+package com.iteso.calendar;
+
+import java.time.LocalTime;
+
+public class DateTime extends Date {
+
+    private int seconds, minutes, hours = 0;
+
+    public DateTime(int day, int month, int year, int format, int hours, int minutes, int seconds) {
+        super(day, month, year, format);
+        setTime(hours, minutes, seconds);
+    }
+
+    public DateTime(int day, int month, int year, int hours, int minutes, int seconds) {
+        super(day, month, year);
+        setTime(hours, minutes, seconds);
+    }
+
+    public DateTime(int hours, int minutes, int seconds) {
+        super();
+        setTime(hours, minutes, seconds);
+    }
+
+    public DateTime() {
+        super();
+        toSystemTime();
+    }
+
+    public DateTime(Date d) {
+        super(d.getDay(), d.getMonth(), d.getYear());
+        toSystemTime();
+    }
+
+    public void toSystemTime() {
+        LocalTime now = LocalTime.now();
+        this.hours = now.getHour();
+        this.minutes = now.getMinute();
+        this.seconds = now.getSecond();
+    }
+
+    public void setTime(int hours, int minutes, int seconds) {
+        setHours(hours);
+        setMinutes(minutes);
+        setSeconds(seconds);
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        if (seconds >= 0 && seconds < 60)
+            this.seconds = seconds;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        if (minutes >= 0 && minutes < 60)
+            this.minutes = minutes;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        if (hours >= 0 && hours < 24)
+            this.hours = hours;
+    }
+
+    public String toString() {
+        if (this.getFormat() == 0)
+            return String.format("[%02d:%02d:%02d] %s", this.hours, this.minutes, this.seconds, super.toString());
+        else {
+            int temphours = this.hours;
+            String AMPM = "AM";
+            if (this.hours >= 12) AMPM = "PM";
+            if (this.hours == 0) temphours = 12;
+            else if (this.hours > 12) temphours -= 12;
+            return String.format("[%02d:%02d:%02d %s] %s", temphours, this.minutes, this.seconds, AMPM, super.toString());
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof DateTime)) return false;
+        DateTime objDT = (DateTime) obj;
+        return super.equals(obj) && this.hours == objDT.getHours() && this.minutes == objDT.getMinutes() && this.seconds == objDT.getSeconds();
+    }
+
+    public DateTime clone() {
+        DateTime clon = new DateTime(getDay(), getMonth(), getYear(), getFormat(), this.hours, this.minutes, this.seconds);
+        return clon;
+    }
+
+    public void next() {
+        seconds += 1;
+        if (seconds == 60) {
+            seconds = 0;
+            minutes += 1;
+            if (minutes == 60) {
+                minutes = 0;
+                hours++;
+                if (hours == 24) {
+                    hours = 0;
+                    super.next();
+                }
+            }
+        }
+    }
+
+    public Date toDate() {
+        return super.clone();
+    }
+
 }
--- src/tests/TestDateTime.java
+++ src/tests/TestDateTime.java
@@ -1,2 +1,38 @@
-package tests;public class TestDateTime {
+package tests;
+
+import com.iteso.calendar.Date;
+import com.iteso.calendar.DateTime;
+
+public class TestDateTime {
+
+    public static void main(String[] args) {
+//      Crea variable time1 que guarde la hora 11:59:59 PM y la fecha 31-Dic-2022
+        DateTime time1 = new DateTime(31, 12, 2022,23, 59, 59);
+//      Crea variable time2 que guarde la hora 12:00:00AM y la fecha 1-Ene-2023
+        DateTime time2 = new DateTime(1, 1, 2023, 0, 0, 0);
+//      Crea variable date3 que guarde la fecha 1-Ene-2023
+        Date date3 = new Date(1, 1, 2023);
+//      Crea una variable time4 que guarde la fecha date3 (utilizar 4to constructor)
+        DateTime time4 = new DateTime(date3);
+//      Crea una variable date5 que sea igual a la fecha de time1 (usar método toDate)
+        Date date5 = time1.toDate();
+//      Súmale un segundo a time1
+        time1.next();
+//      Imprima la comparación entre time1 y time2 en ambos sentidos
+        System.out.println(time1.equals(time2));
+        System.out.println(time2.equals(time1));
+//      Imprima la comparación entre time2 y date3 en ambos sentidos
+        System.out.println(time2.equals(date3));
+        System.out.println(date3.equals(time2));
+//      Almacenar todos los objetos creados en un mismo arreglo ¿de qué tipo será el arreglo?
+        Date[] dates = { time1, time2, date3, time4, date5 };
+//      Utilizando ciclo for-each, imprimir todos los objetos del arreglo en los 3 formatos (4 líneas de código)
+        for(Date d: dates) {
+            for(int f = 0; f <= 2; f ++) {
+                d.setFormat(f);
+                System.out.println(d);
+            }
+        }
+    }
+
 }
